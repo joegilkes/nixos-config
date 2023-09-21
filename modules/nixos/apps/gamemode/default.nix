29c77698cb5ca9f13e41637fc5ddf42aa6f60183
Joe Gilkes <j.gilkes@warwick.ac.uk>
2023-09-21 10:51:45 +0100

    Added config for Gamemode

---
+++ modules/nixos/apps/gamemode/default.nix
@@ -0,0 +1,21 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.pluskinda;
+let 
+  cfg = config.pluskinda.apps.gamemode;
+in
+{
+  options.pluskinda.apps.gamemode = with types; {
+    enable = mkBoolOpt false "Whether or not to enable Gamemode.";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [ 
+      gamemode
+    ];
+    config.pluskinda.desktop.gnome.extensions = with pkgs; [
+      gnomeExtensions.gamemode
+    ];
+  };
+}
