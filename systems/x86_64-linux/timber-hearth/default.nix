76b52e937cb30c45837ba4ab0deb6bece1fb566e
joegilkes <j.gilkes@warwick.ac.uk>
2023-09-30 14:24:12 +0100

    Added config for timber-hearth

---
+++ systems/x86_64-linux/timber-hearth/default.nix
@@ -0,0 +1,25 @@
+{ pkgs, config, lib, channel, ...}:
+
+with lib;
+with lib.pluskinda;
+{
+  imports = [ ./hardware.nix ];
+
+  networking.hostName = "timber-hearth";
+
+  pluskinda = {
+    suites = {
+      common = enabled;
+      desktop = enabled;
+      browsers = enabled;
+      creative = enabled;
+      development = enabled;
+      emulation = enabled;
+      gaming = enabled;
+      media = enabled;
+      social = enabled;
+    };
+  };
+
+  system.stateVersion = "23.05";
+}
---
+++ systems/x86_64-linux/timber-hearth/hardware.nix
@@ -0,0 +1,50 @@
+{ config, lib, pkgs, modulesPath, inputs, ...}:
+
+let
+  inherit (inputs) nixos-hardware;
+in
+{
+  imports = with nixos-hardware.nixosModules; [
+    (modulesPath + "/installer/scan/not-detected.nix")
+    common-cpu-amd
+    common-cpu-amd-pstate
+    common-gpu-amd
+    common-pc
+    common-pc-ssd
+  ];
+
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/89341997-7c37-4a6c-a83d-37ee31770a55";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/8ED7-D52D";
+      fsType = "vfat";
+    };
+
+  fileSystems."/beluga" = 
+    { device = "/dev/disk/by-label/beluga";
+      fsType = "ext4";
+    }
+
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/14e3060c-1d7c-48c7-b164-51044d9eed54"; }
+    ];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp14s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp15s0.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
