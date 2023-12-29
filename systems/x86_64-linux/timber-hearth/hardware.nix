471b6452687c76575be67da984bfc8239f341538
joegilkes <j.gilkes@warwick.ac.uk>
2023-12-29 17:27:57 +0000

    Updated usb-modeswitch attribute in brittle-hollow hardware

--- systems/x86_64-linux/timber-hearth/hardware.nix
+++ systems/x86_64-linux/timber-hearth/hardware.nix
@@ -49,5 +49,5 @@ in
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
-  hardware.usbWwan.enable = true;
+  hardware.usb-modeswitch.enable = true;
 }
