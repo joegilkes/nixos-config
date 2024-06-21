fa92320ee9fa432ac588e844f3964eb907e884df
joegilkes <j.gilkes@warwick.ac.uk>
2024-06-21 20:56:48 +0100

    Updated to rc1 of Lix

--- flake.nix
+++ flake.nix
@@ -12,13 +12,8 @@
     master.url = "github:nixos/nixpkgs/master";
 
     # Replace Nix with Lix https://lix.systems/
-    lix = {
-      url = "git+https://git@git.lix.systems/lix-project/lix?ref=refs/tags/2.90-beta.1";
-      flake = false;
-    };
     lix-module = {
-      url = "git+https://git.lix.systems/lix-project/nixos-module";
-      inputs.lix.follows = "lix";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.90.0-rc1.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
