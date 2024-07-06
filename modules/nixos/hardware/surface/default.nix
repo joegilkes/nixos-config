aa4e7cbf5e3e05fa1d415834f50deda4e5e1bc1f
Joe Gilkes <j.gilkes@warwick.ac.uk>
2024-07-06 16:51:20 +0100

    Fixed broken sound due to libcamera on attlerock

--- modules/nixos/hardware/surface/default.nix
+++ modules/nixos/hardware/surface/default.nix
@@ -8,12 +8,35 @@ in
   options.pluskinda.hardware.surface = with types; {
     enable = mkBoolOpt false
       "Whether or not to enable additional support for Microsoft Surface devices.";
+    useLibcamera = mkBoolOpt false
+      "Whether or not to use libcamera (also enables/disables it in pipewire)";
   };
 
   config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-      libwacom-surface
-      libcamera 
+    environment.systemPackages = with pkgs; mkMerge [
+      [
+        libwacom-surface
+      ]
+      ( mkIf cfg.useLibcamera [
+        libcamera
+      ])
     ];
+
+    services.pipewire.package = mkIf (!cfg.useLibcamera) (
+      pkgs.pipewire.override { libcameraSupport = false; }
+    );
+
+    environment.etc = mkIf (!cfg.useLibcamera) {
+      "wireplumber/wireplumber.conf.d/51-libcamera-disable.conf" = {
+        text = ''
+        wireplumber.profiles = {
+            main = {
+                monitor.libcamera = disabled
+            }
+        }
+        '';
+        mode = "0444";
+      };
+    };
   };
 }
