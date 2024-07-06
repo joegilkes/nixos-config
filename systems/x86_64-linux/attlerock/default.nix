cfdb34d1faca972b3084a383a4c445f775dc2853
joegilkes <j.gilkes@warwick.ac.uk>
2024-07-06 17:42:37 +0100

    Enabled using timber-hearth as a substituter for attlerock

--- systems/x86_64-linux/attlerock/default.nix
+++ systems/x86_64-linux/attlerock/default.nix
@@ -10,6 +10,18 @@ with lib.pluskinda;
   pluskinda = {
     hardware.surface = enabled;
 
+    nix = {
+      # Overwritten by Lix, use nix_2_18_upstream to go back
+      # package = pkgs.nixVersions.stable;
+
+      extra-substituters = {
+        "ssh://joe@timber-hearth".key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINJuGmKSUGo325hD/w/uWN2sBQklwkG06K4v3fsB11O3 joe@timber-hearth";
+      };
+    };
+    networking.extraHosts = ''
+      192.168.0.36 timber-hearth
+    '';
+
     suites = {
       common = enabled;
       desktop = enabled;
