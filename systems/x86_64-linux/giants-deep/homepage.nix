1ed324e4e29f52d09b09ece971dedcd6e9731178
joegilkes <j.gilkes@warwick.ac.uk>
2024-08-27 21:49:36 +0100

    Updated giants-deep Homepage to use agenix secrets

--- systems/x86_64-linux/giants-deep/homepage.nix
+++ systems/x86_64-linux/giants-deep/homepage.nix
@@ -1,134 +1,141 @@
-{ config, lib, pkgs, inputs, ...}:
+{ config, lib, pkgs, inputs, self, ...}:
 
 let 
-  inherit (lib) readFile;
   hname = config.networking.hostName;
 in 
 {
-  services = {
-    homepage-dashboard = {
-      enable = true;
-      openFirewall = true;
-      settings = {
-        title = "NAS Dashboard [${hname}]";
-        headerstyle = "clean";
-      };
-      bookmarks = [{
-        dev = [
-          { github = [{
-              abbr = "GitHub";
-              href = "https://github.com/joegilkes";
-              icon = "github-light.png";
-            }];
-          }
-          { homepage-docs = [{
-              abbr = "HPDocs";
-              href = "https://gethomepage.dev/";
-              icon = "homepage.png";
-            }];
-          }
-        ];
-      }];
-      services = [
-        {
-          media = [
-            { 
-              Jellyfin = {
-                icon = "jellyfin.png";
-                href = "http://${hname}.local:8096";
-                description = "film/TV streaming";
-                widget = {
-                  type = "jellyfin";
-                  url = "http://${hname}.local:8096";
-                  key = readFile "/mnt/gabbro/storage/jellyfin/api.key";
-                };
-              };
-            }
-          ];
-        network = [
-          {
-            "AdGuard Home" = {
-              icon = "";
-              href = "http://${hname}.local:3003";
-              description = "DNS filter";
+  age.secrets = {
+    homepage-env = {
+      file = "${self}/secrets/giants-deep-homepage.age";
+      owner = "root";
+      group = "users";
+      mode = "400";
+    };
+  };
+
+  services.homepage-dashboard = {
+    enable = true;
+    openFirewall = true;
+    environmentFile = config.age.secrets.homepage-env.path;
+    settings = {
+      title = "NAS Dashboard [${hname}]";
+      headerstyle = "clean";
+    };
+    bookmarks = [{
+      dev = [
+        { github = [{
+            abbr = "GitHub";
+            href = "https://github.com/joegilkes";
+            icon = "github-light.png";
+          }];
+        }
+        { homepage-docs = [{
+            abbr = "HPDocs";
+            href = "https://gethomepage.dev/";
+            icon = "homepage.png";
+          }];
+        }
+      ];
+    }];
+    services = [
+      {
+        media = [
+          { 
+            Jellyfin = {
+              icon = "jellyfin.png";
+              href = "http://${hname}.local:8096";
+              description = "film/TV streaming";
               widget = {
-                type = "adguard";
-                url = "http://${hname}.local:3003";
-                username = "joe";
-                password = readFile "/mnt/gabbro/storage/adguardhome/pass";
+                type = "jellyfin";
+                url = "http://${hname}.local:8096";
+                key = "{{HOMEPAGE_VAR_JELLYFIN_API_KEY}}";
               };
             };
           }
         ];
-      }];
-      widgets = [
+      network = [
         {
-          search = {
-            provider = "google";
-            target = "_self";
-          };
-        }
-        {
-          glances = {
-            url = "http://${hname}.local:61208";
-            cpu = true;
-            label = "CPU Usage";
-          };
-        }
-        {
-          glances = {
-            url = "http://${hname}.local:61208";
-            memory = true;
-            label = "Memory Usage";
-          };
-        }
-        {
-          glances = {
-            url = "http://${hname}.local:61208";
-            process = true;
-            label = "Top Processes";
-          };
-        }
-        {
-          glances = {
-            url = "http://${hname}.local:61208";
-            "network:enp0s31f6" = true;
-            label = "Network Usage";
-          };
-        }
-        {
-          glances = {
-            url = "http://${hname}.local:61208";
-            "fs:/mnt/gabbro/public" = true;
-            label = "gabbro/public";
-            chart = false;
-          };
-        }
-        {
-          glances = {
-            url = "http://${hname}.local:61208";
-            "fs:/mnt/gabbro/storage" = true;
-            label = "gabbro/storage";
-            chart = false;
-          };
-        }
-        {
-          glances = {
-            url = "http://${hname}.local:61208";
-            "fs:/mnt/gabbro/backups" = true;
-            label = "gabbro/backups";
-            chart = false;
-          };
-        }
-        {
-          glances = {
-            url = "http://${hname}.local:61208";
-            "fs:/mnt/gabbro/media" = true;
-            label = "gabbro/media";
-            chart = false;
+          "AdGuard Home" = {
+            icon = "";
+            href = "http://${hname}.local:3003";
+            description = "DNS filter";
+            widget = {
+              type = "adguard";
+              url = "http://${hname}.local:3003";
+              username = "{{HOMEPAGE_VAR_ADGUARD_USER}}";
+              password = "{{HOMEPAGE_VAR_ADGUARD_PASS}}";
+            };
           };
         }
       ];
-    };
+    }];
+    widgets = [
+      {
+        search = {
+          provider = "google";
+          target = "_self";
+        };
+      }
+      {
+        glances = {
+          url = "http://${hname}.local:61208";
+          cpu = true;
+          label = "CPU Usage";
+        };
+      }
+      {
+        glances = {
+          url = "http://${hname}.local:61208";
+          memory = true;
+          label = "Memory Usage";
+        };
+      }
+      {
+        glances = {
+          url = "http://${hname}.local:61208";
+          process = true;
+          label = "Top Processes";
+        };
+      }
+      {
+        glances = {
+          url = "http://${hname}.local:61208";
+          "network:enp0s31f6" = true;
+          label = "Network Usage";
+        };
+      }
+      {
+        glances = {
+          url = "http://${hname}.local:61208";
+          "fs:/mnt/gabbro/public" = true;
+          label = "gabbro/public";
+          chart = false;
+        };
+      }
+      {
+        glances = {
+          url = "http://${hname}.local:61208";
+          "fs:/mnt/gabbro/storage" = true;
+          label = "gabbro/storage";
+          chart = false;
+        };
+      }
+      {
+        glances = {
+          url = "http://${hname}.local:61208";
+          "fs:/mnt/gabbro/backups" = true;
+          label = "gabbro/backups";
+          chart = false;
+        };
+      }
+      {
+        glances = {
+          url = "http://${hname}.local:61208";
+          "fs:/mnt/gabbro/media" = true;
+          label = "gabbro/media";
+          chart = false;
+        };
+      }
+    ];
   };
 }
