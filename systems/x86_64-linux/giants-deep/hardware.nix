73907438e3d3ca914afb90b4a168f720f7196b08
joegilkes <j.gilkes@warwick.ac.uk>
2024-09-28 13:23:59 +0100

    Added driver for TBS6902 to giants-deep

--- systems/x86_64-linux/giants-deep/hardware.nix
+++ systems/x86_64-linux/giants-deep/hardware.nix
@@ -14,7 +14,7 @@ in
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelPackages = pkgs.linuxPackages_6_10;
-  boot.kernelModules = [ "coretemp" ];
+  boot.kernelModules = [ "coretemp" "tbs" ];
   boot.kernelParams = [ "nohibernate" ];
   boot.extraModulePackages = [ ];
   boot.supportedFilesystems = [ "zfs" ];
