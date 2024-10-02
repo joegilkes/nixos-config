96dd06e0cfc05b27e54869721a43e147f618394a
joegilkes <j.gilkes@warwick.ac.uk>
2024-10-02 11:01:59 +0100

    Moved to custom Ryujinx package ahead of deprecation within nixpkgs

--- modules/nixos/apps/ryujinx/default.nix
+++ modules/nixos/apps/ryujinx/default.nix
@@ -10,5 +10,5 @@ in
   };
 
   config =
-    mkIf cfg.enable { environment.systemPackages = with pkgs; [ ryujinx ]; };
+    mkIf cfg.enable { environment.systemPackages = with pkgs.pluskinda; [ ryujinx ]; };
 }
