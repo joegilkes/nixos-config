f797e964acfdafb588809743d9a30c4b0830175c
Joe Gilkes <j.gilkes@warwick.ac.uk>
2024-10-08 20:09:15 +0100

    tbs driver overlay fixes 2/?

--- overlays/tbs/default.nix
+++ overlays/tbs/default.nix
@@ -1,25 +1,23 @@
 { channels, ... }:
 
-with channels.nixpkgs;
-let 
-  media = fetchFromGitHub rec {
-    name = repo;
-    owner = "tbsdtv";
-    repo = "linux_media";
-    rev = "a2e856bfb243942a9e9aaae004353501f513a5d7";
-    hash = "";
-  };
-
-  build = fetchFromGitHub rec {
-    name = repo;
-    owner = "tbsdtv";
-    repo = "media_build";
-    rev = "0f49c76b80838ded04bd64c56af9e1f9b8ac1965";
-    hash = "";
-  };
-in 
-  final: prev: {
-    tbs = prev.tbs.overrideAttrs (old: {
-      srcs = [ media build ];
-    });
-  }
+final: prev: {
+  tbs = prev.tbs.overrideAttrs (old: {
+    version = "20241003-${prev.kernel.version}";
+    srcs = [ 
+      prev.fetchFromGitHub rec {
+        name = repo;
+        owner = "tbsdtv";
+        repo = "linux_media";
+        rev = "a2e856bfb243942a9e9aaae004353501f513a5d7";
+        hash = "";
+      }
+      prev.fetchFromGitHub rec {
+        name = repo;
+        owner = "tbsdtv";
+        repo = "media_build";
+        rev = "0f49c76b80838ded04bd64c56af9e1f9b8ac1965";
+        hash = "";
+      }
+    ];
+  });
+}
