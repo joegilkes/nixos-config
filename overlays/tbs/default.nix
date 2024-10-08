81519556561764adb94066bff03d20bf56be682c
Joe Gilkes <j.gilkes@warwick.ac.uk>
2024-10-08 19:53:12 +0100

    Added overlay for tbs driver sources

---
+++ overlays/tbs/default.nix
@@ -0,0 +1,24 @@
+{ channels, ... }:
+
+let 
+  media = fetchFromGitHub rec {
+    name = repo;
+    owner = "tbsdtv";
+    repo = "linux_media";
+    rev = "a2e856bfb243942a9e9aaae004353501f513a5d7";
+    hash = "";
+  };
+
+  build = fetchFromGitHub rec {
+    name = repo;
+    owner = "tbsdtv";
+    repo = "media_build";
+    rev = "0f49c76b80838ded04bd64c56af9e1f9b8ac1965";
+    hash = "";
+  };
+in 
+  final: prev: {
+    tbs = prev.tvs.overrideAttrs (old: {
+      srcs = [ media build ];
+    });
+  }
