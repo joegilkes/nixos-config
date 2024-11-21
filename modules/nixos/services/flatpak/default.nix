367942df490faa4c5647231e7979b5b9130338ae
joegilkes <j.gilkes@warwick.ac.uk>
2024-11-21 16:29:17 +0000

    Added service for populating flatpak remotes

--- modules/nixos/services/flatpak/default.nix
+++ modules/nixos/services/flatpak/default.nix
@@ -12,5 +12,13 @@ in
 
   config = mkIf cfg.enable {
     services.flatpak.enable = true;
+
+    systemd.services.flatpak-repo = {
+      wantedBy = [ "multi-user.target" ];
+      path = [ pkgs.flatpak ];
+      script = ''
+        flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+      '';
+    };
   };
 }
