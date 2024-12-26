0101e88c404882382e0fb0876b3aa83e1a3b68cd
Joe Gilkes <j.gilkes@warwick.ac.uk>
2024-12-26 22:43:50 +0000

    Removed sd-image-aarch64 config from interloper

--- systems/aarch64-linux/interloper/hardware.nix
+++ systems/aarch64-linux/interloper/hardware.nix
@@ -6,12 +6,15 @@ in
 {
   imports = with nixos-hardware.nixosModules; [
     (modulesPath + "/installer/scan/not-detected.nix")
-    (modulesPath + "/installer/sd-card/sd-image-aarch64.nix")
     raspberry-pi-4
   ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "usb_storage" "usbhid" ];
   boot.kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
+  boot.loader = {
+    grub.enable = false;
+    generic-extlinux-compatible.enable = true;
+  };
 
   fileSystems."/" =
     { device = "/dev/disk/by-label/NIXOS_SD";
