234729eee050601fb16b456a92043a192779a9f9
joegilkes <j.gilkes@warwick.ac.uk>
2025-01-26 22:30:53 +0000

    Attempted a custom crowdsec package over crowdsec flake

--- overlays/crowdsec/default.nix
+++ overlays/crowdsec/default.nix
@@ -1,1 +1,10 @@
-{ channels, inputs, ... }: inputs.crowdsec.overlays.default
+{ inputs, lib, ... }:
+let
+  crowdsec-pkg-overlay = final: prev: {
+    crowdsec = prev.pluskinda.crowdsec;
+  };
+in
+  lib.composeManyExtensions [ 
+    inputs.crowdsec.overlays.default
+    crowdsec-pkg-overlay
+  ]
