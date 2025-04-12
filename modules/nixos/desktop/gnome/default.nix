5f8df511f267605cc38c5cc926ac2471b6ffd0a4
joegilkes <j.gilkes@warwick.ac.uk>
2025-04-12 20:18:53 +0100

    Added GStreamer plugins to GNOME config

--- modules/nixos/desktop/gnome/default.nix
+++ modules/nixos/desktop/gnome/default.nix
@@ -73,6 +73,13 @@ in
       gnome-maps
     ]);
 
+    environment.sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 = lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" (with pkgs.gst_all_1; [
+      gst-plugins-good
+      gst-plugins-bad
+      gst-plugins-ugly
+      gst-libav
+    ]);
+
     systemd.tmpfiles.rules = [
       "d ${gdmHome}/.config 0711 gdm gdm"
     ] ++ (
