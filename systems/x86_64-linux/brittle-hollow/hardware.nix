5349ef32d2f3480c9fff6c85e1abd16cda8bdd8d
Joe Gilkes <j.gilkes@warwick.ac.uk>
2025-05-30 16:34:23 +0100

    Updated kernel on brittle-hollow

--- systems/x86_64-linux/brittle-hollow/hardware.nix
+++ systems/x86_64-linux/brittle-hollow/hardware.nix
@@ -9,7 +9,7 @@ in
     dell-latitude-5520
   ];
 
-  boot.kernelPackages = pkgs.linuxPackages_6_11;
+  boot.kernelPackages = pkgs.linuxKernel.packages.linux_xanmod_stable;
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
