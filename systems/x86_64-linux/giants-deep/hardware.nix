823afbbc6f1afa0c7519b0ce7505b05d9d3970f0
joegilkes <j.gilkes@warwick.ac.uk>
2025-08-20 19:52:57 +0100

    Upgraded giants-deep GPU driver to Intel VPL

--- systems/x86_64-linux/giants-deep/hardware.nix
+++ systems/x86_64-linux/giants-deep/hardware.nix
@@ -95,6 +95,6 @@ in
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   hardware.graphics = {
     enable = true;
-    extraPackages = with pkgs; [ intel-media-sdk ];
+    extraPackages = with pkgs; [ vpl-gpu-rt ];
   };
 }
