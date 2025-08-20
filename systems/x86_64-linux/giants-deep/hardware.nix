992495965e76319cfaecc06a142d2da6ae9e5ace
joegilkes <j.gilkes@warwick.ac.uk>
2025-08-20 19:49:09 +0100

    Added extra graphics drivers for giants-deep

--- systems/x86_64-linux/giants-deep/hardware.nix
+++ systems/x86_64-linux/giants-deep/hardware.nix
@@ -93,4 +93,8 @@ in
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [ intel-media-sdk ];
+  };
 }
